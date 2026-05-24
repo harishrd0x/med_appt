6a08591a2985bfb3713f4e6e4f5493c64850fe8a
harishrd0x <harishrd321@gmail.com>
2026-05-24 14:31:29 -0400

    convert static pages to react compoennts

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="">Home</a>
+            <a href="">Appointments</a>
+            <a href="">Sign Up</a>
+            <a href="">Login</a>
+        </div>
+    )
+}
